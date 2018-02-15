f54111e6dbc09d895f7593a3123fd523683af07e
Tim Johnson <pavementii@gmail.com>
2018-02-15 15:49:21 -0500

    added waypoint functionality

--- OmniFood/omnifood/resources/js/script.js
+++ OmniFood/omnifood/resources/js/script.js
@@ -1,9 +1,22 @@
 $(document).ready(function() {
     
-    $('h1').click(function() {
-        $(this).css('background-color', '#ff0000');        
-    })
-    
-    
+   $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '60px;'
+    })    
+       });
 
-});
+
+/*
+var waypoint = new Waypoint({
+  element: document.getElementById('px-offset-waypoint'),
+  handler: function(direction) {
+    notify('I am 20px from the top of the window')
+  },
+  offset: 20 
+}) */
