bcde5bc8d159b77ca49a82056a46ecfaf2620c0b
Tim Johnson <pavementii@gmail.com>
2018-02-16 13:07:22 -0500

    added smooth scrolling and scroll on buttons

--- OmniFood/omnifood/resources/js/script.js
+++ OmniFood/omnifood/resources/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     
+    /* ------ Sticky Navigation ------ */    
    $('.js--section-features').waypoint(function(direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -8,15 +9,34 @@ $(document).ready(function() {
         }
     }, {
       offset: '60px;'
-    })    
-       });
+    });   
+    
+    
+    /* ------ Scroll on Buttons ------ */
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 2000);
+    });
+    
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+    
+    /* ----- Navigation Scroll ----- */
+    $(function(){
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                   if (target.length) {
+                     $('html,body').animate({
+                         scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+    
+});
 
 
-/*
-var waypoint = new Waypoint({
-  element: document.getElementById('px-offset-waypoint'),
-  handler: function(direction) {
-    notify('I am 20px from the top of the window')
-  },
-  offset: 20 
-}) */
