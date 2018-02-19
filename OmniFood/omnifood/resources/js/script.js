3bcdfa5177ce0b92a696d9cace401751c82cb87a
Tim Johnson <pavementii@gmail.com>
2018-02-19 15:23:33 -0500

    added animations attached to waypoints

--- OmniFood/omnifood/resources/js/script.js
+++ OmniFood/omnifood/resources/js/script.js
@@ -36,7 +36,35 @@ $(document).ready(function() {
             }
         });
     });
+   
     
+    
+    /* ----- Animations on scroll ----- */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInLeft');
+    }, {
+        offset: '70%'
+    });
+
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+    
+    
+
+
 });
-
-
